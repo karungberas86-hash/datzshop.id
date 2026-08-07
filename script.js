5e9946aa95b99dc7582b436bd4179f9ca016efe8
datzshop.id <karungberas86@gmail.com>
2026-08-08 02:00:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function copyPayment(number, button) {
+    navigator.clipboard.writeText(number).then(function () {
 
+        const originalText = button.innerText;
+
+        button.innerText = "✓ BERHASIL DISALIN";
+
+        setTimeout(function () {
+            button.innerText = originalText;
+        }, 2000);
+
+    }).catch(function () {
+        alert("Nomor: " + number);
+    });
+}
